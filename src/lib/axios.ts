ddeaa66242e5d49383e9cee50f111dcf7363583d
Pedro Requiao <alonsofts@gmail.com>
2024-08-06 22:54:08 -0400

    test: CI github actions

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 import { env } from '@/env'
+
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: true, // Permite o envio de cookies e cabeçalhos de autenticação
 })
 
 if (env.VITE_ENABLE_API_DELAY) {
