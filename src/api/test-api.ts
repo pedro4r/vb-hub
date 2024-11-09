d377e66d1cd87ceb704a7984e6b7a3ed6f912dd7
Pedro Requiao <alonsofts@gmail.com>
2024-11-08 21:15:30 -0500

    edit: cors 11

--- src/api/test-api.ts
+++ src/api/test-api.ts
@@ -1,8 +1,10 @@
-import { api } from '@/lib/axios'
+import axios from 'axios'
 
 export async function testApi() {
   try {
-    const response = await api.get('/test')
+    const response = await axios.get('https://api.hubdash.space/test', {
+      withCredentials: true,
+    })
     console.log('response', response.data)
   } catch (error) {
     console.error('Error during authentication:', error)
